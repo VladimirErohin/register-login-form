8017590aa1816abae6e8deb509bdd391cc540d1f
VladimirErohin <vovaerohin1@gmail.com>
2023-10-06 22:21:47 +0300

    started to check requests to the postman

--- server/db.js
+++ server/db.js
@@ -1,10 +1,27 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: "postgres",
-    password: "uladzimir88",
-    host: "localhost",
-    port: 5432,
-    database: "nodelogin",
-});
+const {Sequelize}= require('sequelize');
 
-module.exports = pool;
+module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+
+    {
+        dialect:'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+    }
+);
+
+
+
+
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "uladzimir88",
+//     host: "localhost",
+//     port: 5432,
+//     database: "nodelogin",
+// });
+//
+// module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
+require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5050;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req,res)=>{res.send('chek nodemon')})
 
-app.listen(PORT,()=>console.log(`server started on post ${PORT}`));
+app.get('/', (req,res)=>{res.status(200).json({message:'WORKING again!!!'})})
+//app.get('/', (req,res)=>{res.send('HEllo!')})
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+
+        console.log('Connection has been established successfully.');
+
+        app.listen(PORT, () => console.log(`server started on post ${PORT}`))
+    } catch (e) {
+        console.log('Невозможно выполнить подключение к БД: ', e)
+    }
+}
+
+start()
