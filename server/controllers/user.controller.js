13af795c35daad512741b48f5db6cf9322a84223
VladimirErohin <vovaerohin1@gmail.com>
2023-10-11 18:06:16 +0300

    created message of errors on backend

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,18 @@
 const User = require('../models/model');
+const {validationResult} = require('express-validator');
 
 class UserController {
-    async createUser(req, res) {
+    async createUser(req, res, next) {
         try {
+            //const errors = validationResult(req);
+
             const {id, name, email, password, role} = req.body;
+
+            const candidate = await User.findOne({where: {email}})
+            if (candidate) {
+                console.log("errors --- ", "Пользователь с таким email уже существует")
+                return res.json("Пользователь с таким email уже существует")
+            }
             const user = await User.create({id, name, email, password, role})
             return res.json(user)
             //return res.json('ok')
@@ -13,46 +22,49 @@ class UserController {
     }
 
     async getUsers(req, res) {
-        try{
+        try {
             const users = await User.findAll();
             return res.json(users);
-        }catch (e){
+        } catch (e) {
             console.log(e.message)
         }
 
     }
 
     async getUser(req, res) {
-        try{
+        try {
             const id = req.params.id;
             const remoteUser = await User.findOne({where: {id}});
             res.json(remoteUser);
-        }catch (e) {
-           console.log(e.message )
-        }
-    }
-
-    async updateUser(req, res) {
-        try{
-            const {id, name, email} = req.body;
-            const user = await User.update({
-                    name:name,
-                    email:email,
-                },
-                {where: {id:id}}
-            );
-            console.log('user -- ', user)
-            res.json(user);
-        }catch (e) {
+        } catch (e) {
             console.log(e.message)
         }
-
     }
 
+    // async updateUser(req, res) {
+    //     try{
+    //         const {id, name, email} = req.body;
+    //         const user = await User.update({
+    //                 name:name,
+    //                 email:email,
+    //             },
+    //             {where: {id:id}}
+    //         );
+    //         res.json(user);
+    //     }catch (e) {
+    //         console.log(e.message)
+    //     }
+    //
+    // }
+
     async deleteUser(req, res) {
-        const id = req.params.id;
-        const remoteUser = await User.destroy({where: {id}});
-        res.json(remoteUser);
+        try {
+            const id = req.params.id;
+            const remoteUser = await User.destroy({where: {id}});
+            res.json(remoteUser);
+        } catch (e) {
+            console.log(e.message)
+        }
     }
 }
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,7 +5,7 @@ const userController = require('../controllers/user.controller');
 router.post('/user', userController.createUser)
 router.get('/users', userController.getUsers)
 router.get('/user/:id', userController.getUser)
-router.put('/user', userController.updateUser)
+//router.put('/user', userController.updateUser)
 router.delete('/user/:id', userController.deleteUser)
 
 module.exports = router;
